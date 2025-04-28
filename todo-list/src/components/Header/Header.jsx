35c931e22ab845e256311b6a51b12fc9facec038
Omarit82 <123607116+Omarit82@users.noreply.github.com>
2025-04-28 17:01:00 -0300

    Add Logo

--- todo-list/src/components/Header/Header.jsx
+++ todo-list/src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import './Header.css';
 import { Navigate } from '../Navbar/Navigate';
-
+import logo from '../../assets/img/todo.png'
 export const Header = () => {
     return (
         <>
             <header className='d-flex align-items-center justify-content-center' >
+                <img src={logo} alt="App logo" className='me-2' />
                 <h1>To Do</h1>
             </header>
             <Navigate user={"Omarit"} />
