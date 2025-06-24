88f5114d9cf09253f7bdebfcbe12d2539095a9c7
Omarit82 <123607116+Omarit82@users.noreply.github.com>
2025-06-24 20:01:43 -0300

    coniugazioni

--- coniugazioni/src/components/Navegation/Navegation.jsx
+++ coniugazioni/src/components/Navegation/Navegation.jsx
@@ -1,21 +1,23 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router';
+import './navigation.css';
+
 
 export const Navegation = () => {
     return(
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar expand="lg" className="navegador" id="navegador">
             <Container>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="/home">Home</Nav.Link>
-                    <Nav.Link href="/coniugazioni">Coniugazioni</Nav.Link>
-                    <Nav.Link href="/quiz">Quiz</Nav.Link>
-                    <Nav.Link href="/about">About</Nav.Link>
-                </Nav>
-                </Navbar.Collapse>
+                    <Nav className="d-flex justify-content-around">
+                        <Link to="/" className='btn btn-info p-2'>Home</Link>
+                        <Link to="/coniugazioni" className='btn btn-info p-2'>Coniugazioni</Link>
+                        <Link to="/quiz" className='btn btn-info'  >Quiz</Link>
+                        <Link to="/about" className='btn btn-info '>About</Link>
+                    </Nav>
+                </Navbar.Collapse> 
             </Container>
         </Navbar>    
     )
--- coniugazioni/src/components/pages/Coniugazioni.jsx
+++ coniugazioni/src/components/pages/Coniugazioni.jsx
@@ -1,7 +1,20 @@
+import './main.css'
+
 export const Coiugazioni = () => {
+    const handleClick = () =>{
+        
+    }
+
     return(
         <main>
-            <h2>Coniugazioni</h2>
+            <hr />
+            <div className='d-flex align-items-center justify-content-between '>
+                <button onClick={handleClick} className="btn btn-info session">Nuova Sessione</button>
+                <h2 className='text-center'>Verbo Scelto</h2>
+                <div>
+                    <input type="text" name="" id="" className='me-2 scelto' />
+                </div>
+            </div>
         </main>
     )
 }
